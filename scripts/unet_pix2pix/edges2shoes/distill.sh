b220b985e732cc76fa61ea0fc1f89d86fe37c367
WuJie <33804309+WuJie1010@users.noreply.github.com>
2021-09-02 20:25:09 +0800

    update script

--- scripts/unet_pix2pix/edges2shoes/distill.sh
+++ scripts/unet_pix2pix/edges2shoes/distill.sh
@@ -6,7 +6,7 @@ python distill.py --dataroot database/edges2shoes-r \
   --log_dir logs/unet_pix2pix/edges2shoes-r/distill \
   --batch_size 4 --num_teacher 2 \
   --real_stat_path real_stat/edges2shoes-r_B.npz \
-  --teacher_ngf 64 --teacher_ngf_1 16 --student_ngf 16  --norm batch \
-  --teacher_netG unet_256 --teacher_netG_1 unet_deepest_256 --netD multi_n_layers \
+  --teacher_ngf_w 64 --teacher_ngf_d 16 --student_ngf 16  --norm batch \
+  --teacher_netG_w unet_256 --teacher_netG_d unet_deepest_256 --netD multi_n_layers \
   --nepochs 100 --nepochs_decay 100 --n_dis 1 \
   --AGD_weights 1e1,1e4,1e1,1e-5
